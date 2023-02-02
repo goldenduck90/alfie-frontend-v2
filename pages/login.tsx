f3fadab30c357f8dbcca5c9f0c494bed88069eb9
goldenduck90 <goldenduck90919@gmail.com>
2023-02-01 21:01:39 -0500

    wip react-hook-form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,6 @@
 import { gql, useMutation } from "@apollo/client";
 import { Wrapper } from "../src/components/layouts/Wrapper";
-import { IconInput } from "../src/components/inputs/IconInput";
+import { IconHookInput, IconInput } from "../src/components/inputs/IconInput";
 import { Checkbox } from "../src/components/inputs/Checkbox";
 import { Button } from "../src/components/Button";
 import { LockClosedIcon, UserIcon } from "@heroicons/react/solid";
@@ -11,6 +11,10 @@ import Link from "next/link";
 import { useLoginMutation } from "../src/hooks/useLoginMutation";
 import { useRouter } from "next/router";
 import { useCurrentUserStore } from "@src/hooks/useCurrentUser";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback } from "react";
 
 const loginMutation = gql`
   mutation Login($input: LoginInput!) {
@@ -27,6 +31,14 @@ const loginMutation = gql`
   }
 `;
 
+const LoginFormSchema = z.object({
+  email: z.string().email("Please enter a valid email address."),
+  password: z.string().trim().min(1, "Please enter your password."),
+  remember: z.boolean().nullable(),
+});
+
+type LoginForm = z.infer<typeof LoginFormSchema>;
+
 const LoginSchema = Yup.object().shape({
   email: Yup.string()
     .email("Please enter a valid email address.")
@@ -39,6 +51,24 @@ const Login = () => {
   const [login] = useMutation(loginMutation);
   const { mutateAsync } = useLoginMutation();
   const router = useRouter();
+
+  const { control, handleSubmit } = useForm<LoginForm>({
+    defaultValues: {
+      email: "",
+      password: "",
+      remember: false,
+    },
+    resolver: zodResolver(LoginFormSchema),
+  });
+
+  const onSubmission = useCallback(async (form: LoginForm) => {
+    const { email, password, remember } = form;
+    console.log({ form });
+    try {
+      console.log("Made it here brutha");
+    } catch (err) {}
+  }, []);
+
   const loginForm = useFormik({
     initialValues: {
       error: "",
@@ -97,65 +127,69 @@ const Login = () => {
         <img src={"/assets/logo.png"} alt="Alfie" className="w-36" />
       </div>
       <FormikProvider value={loginForm}>
-        <div className="flex flex-col max-w-md px-14 pt-14 pb-10 bg-white rounded-md gap-5">
-          {errors.error && (
-            <div className="text-red-500 text-sm text-center">
-              {errors.error}
+        <form onSubmit={handleSubmit(onSubmission)}>
+          <div className="flex flex-col max-w-md px-14 pt-14 pb-10 bg-white rounded-md gap-5">
+            {errors.error && (
+              <div className="text-red-500 text-sm text-center">
+                {errors.error}
+              </div>
+            )}
+            <div className="pb-2">
+              <IconHookInput
+                control={control}
+                name="email"
+                placeholder="Email address"
+                type="email"
+                disabled={isSubmitting}
+                icon={<UserIcon className="h-5 w-5 text-indigo-800" />}
+              />
             </div>
-          )}
-          <div className="pb-2">
-            <IconInput
-              name="email"
-              placeholder="Email address"
-              type="email"
-              disabled={isSubmitting}
-              icon={<UserIcon className="h-5 w-5 text-indigo-800" />}
-            />
-          </div>
-          <div className="pb-3">
-            <IconInput
-              name="password"
-              placeholder="Password"
-              type="password"
-              disabled={isSubmitting}
-              icon={<LockClosedIcon className="h-5 w-5 text-indigo-800" />}
-            />
-          </div>
-          <div className="pb-3">
-            <Checkbox
-              name="remember"
-              label="Remember me"
-              disabled={isSubmitting}
-            />
-          </div>
-          <div className="pb-3 flex flex-col items-center">
-            <Button
-              title="Login"
-              onPress={submitForm}
-              disabled={isSubmitting}
-              fullWidth
-            />
-            <div className="pt-3">
-              <Link
-                href="/forgot-password"
-                className="font-mulish text-sm text-indigo-800 hover:text-indigo-600"
-              >
-                Forgot Password
-              </Link>
+            <div className="pb-3">
+              <IconHookInput
+                control={control}
+                name="password"
+                placeholder="Password"
+                type="password"
+                disabled={isSubmitting}
+                icon={<LockClosedIcon className="h-5 w-5 text-indigo-800" />}
+              />
+            </div>
+            <div className="pb-3">
+              <Checkbox
+                name="remember"
+                label="Remember me"
+                disabled={isSubmitting}
+              />
+            </div>
+            <div className="pb-3 flex flex-col items-center">
+              <Button
+                title="Login"
+                onPress={submitForm}
+                disabled={isSubmitting}
+                fullWidth
+              />
+              <div className="pt-3">
+                <Link
+                  href="/forgot-password"
+                  className="font-mulish text-sm text-indigo-800 hover:text-indigo-600"
+                >
+                  Forgot Password
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-col border-t border-gray-200">
+              <p className="text-center text-sm text-gray-400 pt-6">
+                Haven&apos;t signed up yet?{" "}
+                <Link
+                  href="/signup"
+                  className="text-indigo-800 hover:text-indigo-600"
+                >
+                  Click here to see if you are eligible for Alfie.
+                </Link>
+              </p>
             </div>
           </div>
-          <div className="flex flex-col border-t border-gray-200">
-            <p className="text-center text-sm text-gray-400 pt-6">
-              Haven&apos;t signed up yet?{" "}
-              <Link
-                href="/signup"
-                className="text-indigo-800 hover:text-indigo-600"
-              >
-                Click here to see if you are eligible for Alfie.
-              </Link>
-            </p>
-          </div>
-        </div>
+        </form>
       </FormikProvider>
     </Wrapper>
   );
--- src/components/inputs/IconInput.tsx
+++ src/components/inputs/IconInput.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useField } from "formik";
+import { Control, useController } from "react-hook-form";
 
 export const IconInput = ({
   name,
@@ -40,3 +41,49 @@ export const IconInput = ({
     </>
   );
 };
+
+export function IconHookInput({
+  name,
+  icon,
+  placeholder,
+  disabled,
+  control,
+}: {
+  name: string;
+  icon: JSX.Element;
+  control: Control<any>;
+} & React.InputHTMLAttributes<HTMLInputElement>) {
+  const {
+    field,
+    fieldState: { invalid },
+    formState,
+  } = useController({
+    name,
+    control,
+  });
+
+  return (
+    <>
+      <div
+        className={`${
+          invalid ? "border-red-500" : "border-gray-300"
+        } relative w-full border rounded-sm`}
+      >
+        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+          {icon}
+        </span>
+        <input
+          {...field}
+          className="font-mulish w-full py-2 rounded-sm pl-10 appearance-none focus:outline-none placeholder-gray-400 bg-white text-black"
+          placeholder={placeholder}
+          disabled={disabled}
+        />
+      </div>
+      {invalid && (
+        <span className="text-red-500 text-sm">
+          {formState?.errors?.[name] as string}
+        </span>
+      )}
+    </>
+  );
+}
