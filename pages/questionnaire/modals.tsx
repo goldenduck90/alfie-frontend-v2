d4ef716fa2d55b5d9ab40aa174e1f27bf397b910
goldenduck90 <goldenduck90919@gmail.com>
2023-02-09 22:39:15 -0500

    temporary page for building modals

---
+++ pages/questionnaire/modals.tsx
@@ -0,0 +1,37 @@
+import { QuestionnaireLayout } from "@src/components/layouts/QuestionaireLayout";
+import { DialogModal } from "@src/components/modal/Dialog";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-y-3 bg-white shadow-md border rounded-md py-3">
+      <DialogModal
+        text="Medical Questionnaire"
+        title="Medical Questionnaire"
+        description="Complete a basic medical form so that we can tailor our services to your needs."
+      />
+      <DialogModal
+        text="Metabolic Profile"
+        title="Metabolic Profile (Feeling) Questionnaire"
+        description="These questions, in combination with the Metabolic Profiling kit sent to you, help us determine your metabolic profile in order to understand which medications will be most effective for you. Once we get the results of your metabolic kit we’ll share a detailed report on your personal metabolic profile!"
+      />
+      <DialogModal
+        text="Gastrointestinal Symptoms Rating Scale"
+        title="Gastrointestinal Symptoms Rating Scale"
+        description="This survey contains questions about how you have been feeling and what it has been like during the last week."
+      />
+      <DialogModal
+        text="The Three-Factor Eating Questionnaire"
+        title="The Three-Factor Eating Questionnaire"
+        description="We would like to know your habits and behaviours regarding eating and taking meals."
+      />
+    </div>
+  );
+}
+
+Page.getLayout = (page: React.ReactNode) => (
+  <QuestionnaireLayout title="Medical Questionnaire">
+    {page}
+  </QuestionnaireLayout>
+);
+
+Page.isAuthRequired = true;
---
+++ src/components/modal/Dialog.tsx
@@ -0,0 +1,88 @@
+import {
+  CalculatorIcon,
+  CalendarIcon,
+  ClockIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import * as RadixDialog from "@radix-ui/react-dialog";
+import { Button } from "../ui/Button";
+
+export function DialogModal({
+  text,
+  title,
+  description,
+}: {
+  text: string;
+  title: string;
+  description: string;
+}) {
+  return (
+    <RadixDialog.Root>
+      <RadixDialog.Trigger className="text-black text-lg">
+        {text}
+      </RadixDialog.Trigger>
+      <RadixDialog.Portal>
+        <RadixDialog.Overlay className="fixed inset-0 bg-black bg-opacity-60" />
+        <RadixDialog.Content className="bg-white fixed top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full max-w-[420px] max-h-1/2 shadow-md py-6 px-6 rounded-md flex flex-col gap-y-3">
+          <div className="w-full flex relative">
+            <div className="p-2 rounded-md bg-orange-100 stroke-orange-500 max-w-fit mx-auto">
+              <CalculatorIcon className="w-5 h-5 stroke-inherit" />
+            </div>
+            <RadixDialog.Close className="absolute right-0 inset-y " asChild>
+              <button>
+                <XIcon className="w-5 h-5" />
+              </button>
+            </RadixDialog.Close>
+          </div>
+          <DialogModal.Title>{title}</DialogModal.Title>
+          <DialogModal.Description>{description}</DialogModal.Description>
+          <Graybox />
+          <div className="py-4 flex items-center justify-center">
+            <Button size="medium">Start the questionnaire</Button>
+          </div>
+          <p className="text-center text-sm text-gray-700">
+            By taking this survey you accept our Terms & Conditions.
+          </p>
+        </RadixDialog.Content>
+      </RadixDialog.Portal>
+    </RadixDialog.Root>
+  );
+}
+
+DialogModal.Title = function Title({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <RadixDialog.Title className="text-center font-bold">
+      {children}
+    </RadixDialog.Title>
+  );
+};
+DialogModal.Description = function Description({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <RadixDialog.Description className="text-center text-[#475569] text-sm">
+      {children}
+    </RadixDialog.Description>
+  );
+};
+
+function Graybox() {
+  return (
+    <div className="w-full py-4 px-2 rounded-md flex gap-x-4 items-center justify-center bg-gray-100 text-sm">
+      <div className="flex gap-x-1 items-center">
+        <ClockIcon className="w-6 h-6" />
+        <p>15 minutes</p>
+      </div>
+      <div className="flex gap-x-1 items-center">
+        <CalendarIcon className="w-6 h-6" />
+        <p>Assigned 3 days ago</p>
+      </div>
+    </div>
+  );
+}
