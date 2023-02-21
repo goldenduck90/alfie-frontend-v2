561082eaa188d42735ccad46373119852f95de66
goldenduck90 <goldenduck90919@gmail.com>
2023-02-21 07:43:28 -0500

    indiviual patients

--- src/components/patient/tabs/IndividualPatientTabs.tsx
+++ src/components/patient/tabs/IndividualPatientTabs.tsx
@@ -1,13 +1,6 @@
-import { Tab } from "@headlessui/react";
-import { ChevronRightIcon } from "@heroicons/react/outline";
 import * as Tabs from "@radix-ui/react-tabs";
-import { Patient } from "@src/components/practitioner/dashboard/Table";
 import { AvatarInitial } from "@src/components/ui/AvatarInitial";
-import {
-  createColumnHelper,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+
 import { useMemo, useState } from "react";
 
 const TabList = [
@@ -20,7 +13,7 @@ const TabList = [
 
 export function IndividualPatientTabs({ user }: { user: any }) {
   const [activeTab, setActiveTab] = useState(TabList[0]);
-
+  console.log({ user });
   return (
     <div className="flex flex-col overflow-y-auto h-[73vh] w-full bg-white shadow-md rounded-md px-4 py-4">
       <Tabs.Root value={activeTab} onValueChange={setActiveTab}>
@@ -33,18 +26,28 @@ export function IndividualPatientTabs({ user }: { user: any }) {
             ))}
           </Tabs.List>
         </div>
-        <Tabs.Content value={TabList[0]}>
+        <Tabs.Content value={TabList[0]} className="mt-6">
+          <TableInformationHeader user={user} />
           <TableUserObject user={user} />
         </Tabs.Content>
         <Tabs.Content value={TabList[1]}>
           <p>Tasks</p>
+        </Tabs.Content>
+        <Tabs.Content value={TabList[2]}>
+          <TableUserObject
+            user={{
+              WeightLossAttempt: "Several Years",
+              WeightManagement: "Diet",
+              Conditions: "Diabetes",
+            }}
+          />
         </Tabs.Content>
       </Tabs.Root>
     </div>
   );
 }
 
-function TableUserObject({ user }: { user: any }) {
+function TableInformationHeader({ user }: { user: any }) {
   const initials = useMemo(() => {
     if (!user?.name) return "";
     const splitName = user?.name?.split(" ");
@@ -53,19 +56,24 @@ function TableUserObject({ user }: { user: any }) {
     return `${firstInitial || ""}${lastInitial || ""}`;
   }, [user]);
 
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex gap-x-3 items-center">
+        <AvatarInitial index={0} text={initials} />
+        <p>{user?.name}</p>
+      </div>
+      <div className="flex gap-x-3">
+        <p>Active Tasks 0</p>
+        <p>Appointments 0</p>
+      </div>
+    </div>
+  );
+}
+
+function TableUserObject({ user }: { user: any }) {
   if (!user) return null;
   return (
-    <div className="mt-6">
-      <div className="flex items-center justify-between">
-        <div className="flex gap-x-3 items-center">
-          <AvatarInitial index={0} text={initials} />
-          <p>{user?.name}</p>
-        </div>
-        <div className="flex gap-x-3">
-          <p>Active Tasks 0</p>
-          <p>Appointments 0</p>
-        </div>
-      </div>
+    <div className="">
       <div className="min-w-full mt-6 border border-gray-200 rounded-md divide-y divide-y-gray-300">
         {Object.keys(user).map((key) => {
           if (typeof user[key] !== "string") {
@@ -74,7 +82,7 @@ function TableUserObject({ user }: { user: any }) {
           return (
             <div className="flex gap-x-4 px-2 py-2">
               <p className="capitalize min-w-[225px]">{key}</p>
-              <p>{user[key]}</p>
+              <p className="text-gray-600">{user[key]}</p>
             </div>
           );
         })}
