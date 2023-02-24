611dfce135c3244c63163078cec93bd29a73525d
goldenduck90 <goldenduck90919@gmail.com>
2023-02-24 01:03:18 -0500

    basic UI tables for settings

--- src/components/settings/SettingsView.tsx
+++ src/components/settings/SettingsView.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { Layout } from "@src/components/layouts/Layout";
 import { useRouter } from "next/router";
 import * as Tabs from "@radix-ui/react-tabs";
 import { TabTitle } from "@src/components/ui/tabs/TabTitle";
+import { useUserSession } from "@src/hooks/useUserSession";
+import { useUserStateContext } from "@src/context/SessionContext";
+import { useCurrentUserStore } from "@src/hooks/useCurrentUser";
+import { Button } from "../ui/Button";
 export function SettingsView() {
   const router = useRouter();
   const activeTab =
@@ -38,15 +42,103 @@ export function SettingsView() {
           </Tabs.List>
         </div>
         <Tabs.Content value="account-details" className="mt-6">
-          <div>Account Details</div>
+          <AccountDetails />
         </Tabs.Content>
-        <Tabs.Content value="plan-&-billing">
+        <Tabs.Content value="plan-&-billing" className="mt-6">
           <div>Plan & Billing</div>
         </Tabs.Content>
-        <Tabs.Content value="notifications">
+        <Tabs.Content value="notifications" className="mt-6">
           <div>notifications</div>
         </Tabs.Content>
       </Tabs.Root>
+    </div>
+  );
+}
+
+function AccountDetails() {
+  const { user } = useCurrentUserStore();
+  return (
+    <div>
+      <div>
+        <h1 className="text-xl font-[600]">Account Details</h1>
+        <TableView
+          obj={{
+            ["Fullname"]: user?.name || "",
+            ["Email address"]: user?.email || "",
+          }}
+        />
+      </div>
+      <div className="mt-6">
+        <h1 className="text-xl font-[600]">Change password</h1>
+        <TableViewRow
+          inputs={[
+            {
+              left: (
+                <div className="flex flex-col gap-y-2">
+                  <p className="text-lg font-bold">Password</p>
+                  <p>{`You haven't change your password recently`}</p>
+                </div>
+              ),
+              right: (
+                <div>
+                  <Button buttonType="secondary">Change password</Button>
+                </div>
+              ),
+            },
+          ]}
+        />
+      </div>
+      <div className="mt-6">
+        <h1 className="text-xl font-[600]">Language and region</h1>
+        <TableView
+          obj={{
+            ["Automatic time zone"]: "",
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function TableView({ obj }: { obj: Record<string, string> }) {
+  if (!obj) return null;
+  return (
+    <div className="">
+      <div className="min-w-full mt-6 border border-gray-200 rounded-md divide-y divide-y-gray-300">
+        {Object.keys(obj).map((key) => {
+          if (typeof obj[key] !== "string") {
+            return null;
+          }
+          return (
+            <div key={key} className="flex gap-x-4 px-6 py-4">
+              <p className="capitalize min-w-[275px] font-bold">{key}</p>
+              <p className="text-gray-600">{obj[key]}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function TableViewRow({
+  inputs,
+}: {
+  inputs: { left: ReactNode; right?: ReactNode }[];
+}) {
+  if (!inputs) return null;
+  return (
+    <div className="">
+      <div className="min-w-full mt-6 border border-gray-200 rounded-md divide-y divide-y-gray-300">
+        {inputs.map(({ left, right }, index) => {
+          return (
+            <div className="flex justify-between items-center px-6 py-4">
+              {left && left}
+              {right && right}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
