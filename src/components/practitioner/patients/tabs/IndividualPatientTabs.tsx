993a3826916ce1c05bc5995eaea98cc786880c7e
goldenduck90 <goldenduck90919@gmail.com>
2023-02-28 04:24:07 -0500

    alerts item update

--- src/components/practitioner/patients/tabs/IndividualPatientTabs.tsx
+++ src/components/practitioner/patients/tabs/IndividualPatientTabs.tsx
@@ -221,8 +221,9 @@ function AlertItem({ isAcknowledged }: { isAcknowledged?: boolean }) {
           </p>
         </div>
       </div>
-      <div className="flex items-start">
-        <p>3 hours ago</p>
+      <div className="flex items-start justify-start gap-x-2">
+        <CalendarIcon className="w-5 h-5" />
+        <p className="text-sm text-gray-500">3 hours ago</p>
       </div>
       <div className="flex items-center gap-x-3 min-w-[200px] justify-end">
         {isAcknowledged ? (
