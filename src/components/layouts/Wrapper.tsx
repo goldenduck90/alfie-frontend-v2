f56b9699c4e3522e958d22acfe4a4d1f0e8290f2
goldenduck90 <goldenduck90919@gmail.com>
2023-05-22 17:55:14 -0400

    Apply font-mulish in wrapper

--- src/components/layouts/Wrapper.tsx
+++ src/components/layouts/Wrapper.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Wrapper: React.FC<Props> = ({ title, children }) => {
   return (
-    <div className="min-h-screen relative">
+    <div className="min-h-screen relative font-mulish">
       <div className="absolute top-0 left-0 w-full h-1/3 bg-brand-berry z-[-1]" />
       <div className="absolute left-0 w-full p-4">
         <div className="my-4 sm:my-10 h-full flex flex-col items-center">
