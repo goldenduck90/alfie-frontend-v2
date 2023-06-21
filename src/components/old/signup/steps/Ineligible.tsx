6563fcbceadadf43d16be7da5c1db53e1823ae11
goldenduck90 <goldenduck90919@gmail.com>
2023-06-21 11:15:02 -0400

    Add return to joinalfie.com on ineligible page

--- src/components/old/signup/steps/Ineligible.tsx
+++ src/components/old/signup/steps/Ineligible.tsx
@@ -8,6 +8,14 @@ export const Ineligible = () => {
     <Wrapper>
       <div className="flex flex-col px-1 sm:px-14 pt-10 pb-10 bg-white rounded-md space-y-5 min-w-full md:min-w-0 max-w-lg">
         <p className=" text-gray-900">{message}</p>
+        <div className="pt-3 text-center">
+          <a
+            href="https://joinalfie.com"
+            className="text-sm text-brand-berry hover:text-brand-berry-tint-1"
+          >
+            Return to joinalfie.com
+          </a>
+        </div>
       </div>
     </Wrapper>
   );
