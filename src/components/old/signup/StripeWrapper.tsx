00549bd20a538af86dfbf5317bf409a102a37144
goldenduck90 <goldenduck90919@gmail.com>
2023-06-28 17:17:57 -0400

    Optavia signup stripe checkout fix

--- src/components/old/signup/StripeWrapper.tsx
+++ src/components/old/signup/StripeWrapper.tsx
@@ -1,22 +1,10 @@
-import { useMemo } from "react";
-import { gql, useQuery } from "@apollo/client";
 import * as Sentry from "@sentry/react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Loading } from "../Loading";
-
-const getCheckoutQuery = gql`
-  query GetCheckout($id: String!) {
-    checkout(id: $id) {
-      checkout {
-        _id
-        stripeClientSecret
-      }
-    }
-  }
-`;
+import { useCheckoutQuery } from "@src/hooks/useCheckoutQuery";
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -28,15 +16,8 @@ export const StripeWrapper = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const id = router.query.id;
 
-  const {
-    data,
-    loading: fetching,
-    error,
-  } = useQuery(getCheckoutQuery, {
-    variables: {
-      id,
-    },
-  });
+  const { loading, error, stripeClientSecret } = useCheckoutQuery(id);
+
   useEffect(() => {
     // If there is an error with the query, we want to log it to Sentry
     if (error) {
@@ -49,25 +30,21 @@ export const StripeWrapper = ({ children }: { children: React.ReactNode }) => {
     }
   }, [error]);
 
-  const options = useMemo(
-    () => ({
-      clientSecret: data?.checkout?.checkout?.stripeClientSecret,
-    }),
-    [data]
-  );
-
   if (!id) {
     router.push("/signup");
   }
 
-  if (fetching) return <Loading />;
+  if (loading) return <Loading />;
 
   if (error) {
     router.push("/");
   }
 
   return (
-    <Elements stripe={stripePromise} options={options}>
+    <Elements
+      stripe={stripePromise}
+      options={{ clientSecret: stripeClientSecret }}
+    >
       {children}
     </Elements>
   );
