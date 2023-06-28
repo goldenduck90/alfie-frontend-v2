2773e3fab899001c66126819abf47addb05ec390
goldenduck90 <goldenduck90919@gmail.com>
2023-06-28 19:12:10 -0400

    stripe wrapper fix

--- src/components/old/signup/StripeWrapper.tsx
+++ src/components/old/signup/StripeWrapper.tsx
@@ -1,10 +1,22 @@
+import { useMemo } from "react";
+import { gql, useQuery } from "@apollo/client";
 import * as Sentry from "@sentry/react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Loading } from "../Loading";
-import { useCheckoutQuery } from "@src/hooks/useCheckoutQuery";
+
+const getCheckoutQuery = gql`
+  query GetCheckout($id: String!) {
+    checkout(id: $id) {
+      checkout {
+        _id
+        stripeClientSecret
+      }
+    }
+  }
+`;
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -16,8 +28,15 @@ export const StripeWrapper = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const id = router.query.id;
 
-  const { loading, error, stripeClientSecret } = useCheckoutQuery(id);
-
+  const {
+    data,
+    loading: fetching,
+    error,
+  } = useQuery(getCheckoutQuery, {
+    variables: {
+      id,
+    },
+  });
   useEffect(() => {
     // If there is an error with the query, we want to log it to Sentry
     if (error) {
@@ -30,21 +49,25 @@ export const StripeWrapper = ({ children }: { children: React.ReactNode }) => {
     }
   }, [error]);
 
+  const options = useMemo(
+    () => ({
+      clientSecret: data?.checkout?.checkout?.stripeClientSecret,
+    }),
+    [data]
+  );
+
   if (!id) {
     router.push("/signup");
   }
 
-  if (loading) return <Loading />;
+  if (fetching) return <Loading />;
 
   if (error) {
     router.push("/");
   }
 
   return (
-    <Elements
-      stripe={stripePromise}
-      options={{ clientSecret: stripeClientSecret }}
-    >
+    <Elements stripe={stripePromise} options={options}>
       {children}
     </Elements>
   );
