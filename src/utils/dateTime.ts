70277ac5487bd87bf71460e94e2446388562c3c3
goldenduck90 <goldenduck90919@gmail.com>
2023-10-11 10:00:48 -0400

    Create alert tab under patient details screen

--- src/utils/dateTime.ts
+++ src/utils/dateTime.ts
@@ -45,3 +45,30 @@ const isWorkingHour = () => {
 export const isOfficeAvailable = () => {
   return !isHoliday() && isWorkingHour();
 };
+
+export const timeSince = (date: Date) => {
+  var seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+};
