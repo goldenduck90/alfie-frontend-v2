8c81b793234189070091e20a6544ffaae785e4da
goldenduck90 <goldenduck90919@gmail.com>
2023-10-19 16:21:13 -0400

    update

--- src/components/insurance/InsuranceCheck.tsx
+++ src/components/insurance/InsuranceCheck.tsx
@@ -31,7 +31,6 @@ const insuranceTextractMutation = gql`
   mutation insuranceTextract($s3Key: String!) {
     insuranceTextract(s3Key: $s3Key) {
       insurance {
-        company
         type
         memberId
         groupId
@@ -143,12 +142,6 @@ const InsuranceCheck = () => {
           if (data?.insuranceTextract.insurance) {
             const parsedInsurance = data.insuranceTextract.insurance
 
-            // find insurance ID from list
-            if (parsedInsurance.company) {
-              const insuranceId = insData?.insurances.find((i) => i.name === parsedInsurance.company)?._id
-              if (insuranceId) setInsuranceId(insuranceId)
-            }
-
             if (parsedInsurance.type) setType(parsedInsurance.type)
             if (parsedInsurance.memberId) setMemberId(parsedInsurance.memberId)
             if (parsedInsurance.groupId) setGroupId(parsedInsurance.groupId)
@@ -223,14 +216,23 @@ const InsuranceCheck = () => {
         {errors.length > 0 ? (
           <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mx-6 rounded relative" role="alert">
             <div>
-              <strong className="font-bold">Error Processing Insurance!</strong>
+              <strong className="font-bold">An error occured processing your insurance</strong>
               <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
                 <svg className="fill-current h-6 w-6 text-red-500" onClick={() => setErrors([])} role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
               </span>
             </div>
             <div className="ml-4 mt-2">
               <ul className="list-disc">
-                {errors.map((e) => <li>{e}</li>)}
+                {errors.map((e) => {
+
+                  if (e === "INSURANCE_STATE_NOT_ACCEPTED" || e === "INSURANCE_TYPE_NOT_ACCEPTED" || e === "INSURANCE_NO_PROVIDERS") {
+                    return <li>Unfortunately, we dont accept your insurance at this time. <Link href={`/signup/checkout/${checkoutId}`} className="font-bold text-brand-berry">Click here to continue without insurance.</Link></li>
+                  }
+
+                  return (
+                    <li>{e}</li>
+                  )
+                })}
               </ul>
             </div>
           </div>
